76cff5473527f7c69d6974eb49a5913659c125ef
Muhammed Sinan CP <cpmuhammedsinan@gmail.com>
2023-12-10 03:10:41 +0530

    Successfully Completed

--- lib/secrets/api_key.dart
+++
@@ -1,8 +0,0 @@
-class APIEndPoints {
-//https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=8ba5a0b7fa9ffaf0ad261e05e9e4b573&units=metric
-
-  final String cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-  final apikey = "8ba5a0b7fa9ffaf0ad261e05e9e4b573";
-  final unit = "&units=metric";
-}
-// const apikey = "8ba5a0b7fa9ffaf0ad261e05e9e4b573";
